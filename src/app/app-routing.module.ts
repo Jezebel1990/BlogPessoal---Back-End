079a004c589c9c9729cc9d051ede3e4e3120c0a9
Jezebel1990 <jezebelguedes@gmail.com>
2021-06-01 14:19:16 -0300

    Consumo de API - Login e Cadastro

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { InicioComponent } from './inicio/inicio.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+
+  {path: 'inicio', component: InicioComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+  title = 'Olá Mundo!';
+}
